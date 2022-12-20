a1510efe5e0cc071676f21b3ecdbda70e5330cc3
Henning Dodenhof <henning@hdodenhof.de>
2022-12-20 19:48:03 +0100

    Update Solaar panel icons

---
+++ src/solaar-icons.sh
@@ -0,0 +1,12 @@
+inkscape ./apps/solaar.svg --export-dpi 96 -i rect16x16 -o ../usr/share/icons/Mint-Y/apps/16/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/16/solaar.png
+inkscape ./apps/solaar.svg --export-dpi 192 -i rect16x16 -o ../usr/share/icons/Mint-Y/apps/16@2x/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/16@2x/solaar.png
+inkscape ./apps/solaar.svg --export-dpi 96 -i rect22x22 -o ../usr/share/icons/Mint-Y/apps/22/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/22/solaar.png
+inkscape ./apps/solaar.svg --export-dpi 192 -i rect22x22 -o ../usr/share/icons/Mint-Y/apps/22@2x/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/22@2x/solaar.png
+inkscape ./apps/solaar.svg --export-dpi 96 -i rect24x24 -o ../usr/share/icons/Mint-Y/apps/24/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/24/solaar.png
+inkscape ./apps/solaar.svg --export-dpi 192 -i rect24x24 -o ../usr/share/icons/Mint-Y/apps/24@2x/solaar.png
+optipng -quiet -o7 ../usr/share/icons/Mint-Y/apps/24@2x/solaar.png
